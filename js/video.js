e598b289c1e11e3be9210f1d5471cc3052c90f91
Bianca Hiew <hiewb@umich.edu>
2023-11-06 09:30:06 -0500

    added

--- js/video.js
+++ js/video.js
@@ -1,11 +1,78 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+    console.log("Good job opening the window");
 
+    video = document.querySelector("#player1");
+    console.log(video);
+
+    video.autoplay = false;
+    video.loop = false;
+
+    document.getElementById("volume").innerHTML = video.volume * 100 + "%";
+
+    // Play Button
+    document.querySelector("#play").addEventListener("click", function() {
+        video.play();
+        updateVolumeInfo();
+    });
+
+    // Pause Button
+    document.querySelector("#pause").addEventListener("click", function() {
+        video.pause();
+    });
+
+    // Slow Down Button
+    document.querySelector("#slower").addEventListener("click", function() {
+        video.playbackRate -= 0.1;
+        console.log("New speed: " + video.playbackRate.toFixed(2));
+    });
+
+    // Speed Up Button
+    document.querySelector("#faster").addEventListener("click", function() {
+        video.playbackRate += 0.1;
+        console.log("New speed: " + video.playbackRate.toFixed(2));
+    });
+
+    // Skip Ahead Button
+    document.querySelector("#skip").addEventListener("click", function() {
+        if (video.currentTime + 10 >= video.duration) {
+            video.currentTime = 0;
+        } else {
+            video.currentTime += 10;
+        }
+        console.log("Current location: " + video.currentTime.toFixed(2));
+    });
+
+    // Mute Button
+    document.querySelector("#mute").addEventListener("click", function() {
+        if (video.muted) {
+            video.muted = false;
+            document.querySelector("#mute").textContent = "Mute";
+        } else {
+            video.muted = true;
+            document.querySelector("#mute").textContent = "Unmute";
+        }
+    });
+
+    // Volume Slider
+    document.querySelector("#slider").addEventListener("input", function() {
+        video.volume = this.value / 100;
+        updateVolumeInfo();
+    });
+
+    // Old School Button
+    document.querySelector("#vintage").addEventListener("click", function() {
+        video.classList.add("oldSchool");
+    });
+
+    // Original Button
+    document.querySelector("#orig").addEventListener("click", function() {
+        video.classList.remove("oldSchool");
+    });
+
+    // Function to update volume information
+    function updateVolumeInfo() {
+        document.querySelector("#volume").textContent = (video.volume * 100).toFixed(0) + "%";
+    }
 });
-
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
